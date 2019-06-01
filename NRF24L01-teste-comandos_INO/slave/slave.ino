870720ee558672dca3d773cefe6eff27de21b102
TavaresJoao <joao_stvares@outlook.com>
2019-05-31 21:40:05 -0300

    NRF24L01 teste update

---
+++ NRF24L01-teste-comandos_INO/slave/slave.ino
@@ -0,0 +1,113 @@
+/*  NRF24L01 teste comandos - slave
+ *  Joao Tavares
+ *  2019 May 31th
+ */
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(9,10);
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+#define bot_stop          0
+#define bot_foward        1
+#define bot_backward      2
+#define bot_clockwise     4
+#define bot_anticlockwise 5
+#define bot_right         6
+#define bot_left          7
+
+void setup(void)
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  
+  radio.begin();
+  radio.setRetries(15,15);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.startListening();
+  radio.printDetails();
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+  radio.startListening();
+}
+
+void loop(void)
+{
+  if ( radio.available() )
+  {
+    uint16_t data = 0;
+    radio.read( &data, sizeof(uint16_t) );
+
+    if(data == bot_stop)
+    {
+      digitalWrite(2, HIGH);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(7, LOW);
+    }
+    else if(data == bot_foward)
+    {
+      digitalWrite(2, LOW);
+      digitalWrite(3, HIGH);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(7, LOW);
+    }
+    else if(data == bot_backward)
+    {
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(7, LOW);
+    }
+    else if(data == bot_clockwise)
+    {
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, LOW);
+      digitalWrite(7, LOW);
+    }
+    else if(data == bot_anticlockwise)
+    {
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, HIGH);
+      digitalWrite(7, LOW);
+    }
+    else if(data == bot_right)
+    {
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(7, HIGH);
+    }
+    else if(data == bot_left){}
+    else
+    {
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(7, LOW);
+    }
+
+    delay(10);
+  }
+}
